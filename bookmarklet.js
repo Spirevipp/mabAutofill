39370acc675208f672dd5d0a2f3bc88dfac189de
Spirevipp <bjornar.helgeland@hotmail.com>
2020-06-08 00:36:17 +0200

    Forbered flytt script til å bli kjørt fra source

---
+++ bookmarklet.js
@@ -0,0 +1,11 @@
+// forandre script håndtering til å bli lastet inn fra github source, deretter kjørt
+// gjør det enklere å oppdatere script, og slipepr et script på flere hundre ord som en bookmarklet
+
+// pseudo kode
+
+/*
+
+mabAutofillScript = httpget https://raw.githubusercontent.com/Spirevipp/mabAutofill/master/3c%20autofill.js
+set mabAutofillScript as javascript sourcefile
+runscript()
+*/
