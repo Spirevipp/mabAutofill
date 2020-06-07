241aaf49691af405414e55fcc68622350ce544b7
Spirevipp <bjornar.helgeland@hotmail.com>
2020-06-08 00:37:02 +0200

    typo

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 // forandre script håndtering til å bli lastet inn fra github source, deretter kjørt
-// gjør det enklere å oppdatere script, og slipepr et script på flere hundre ord som en bookmarklet
+// gjør det enklere å oppdatere script, og slipper et script på flere hundre ord som en bookmarklet
 
 // pseudo kode
 
